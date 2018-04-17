04071f4abd8749cbf634985c57150da1ad067572
Tero Karvinen <teropkg@gmx.com>
2018-04-17 17:36:14 +0300

    Comment run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-echo "Configuring live USB..."
-setxkbmap fi
-sudo timedatectl set-timezone Europe/Helsinki
+echo "Sirotin Live USB configuration Starting... terokarvinen/sirotin"
+set -o verbose
+
+echo "Installing Salt and Git..."
 sudo apt-get update
 sudo apt-get -y install git salt-minion
+
+echo "Retrieving and Applying Settings..."
 echo 'master: localhost'|sudo tee /etc/salt/minion
 git clone https://github.com/terokarvinen/sirotin
 cd sirotin/
 ./highstate.sh
+
+echo "Configuring Live Desktop"
+setxkbmap fi
+sudo timedatectl set-timezone Europe/Helsinki
+git config --global credential.helper "cache --timeout=3600"
+
 echo "More tips on http://terokarvinen.com/tag/saltstack"
 echo "Done."
