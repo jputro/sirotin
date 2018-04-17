fa1250de0cc04629c74ba5d0337ce0e2acb1df76
Tero Karvinen <teropkg@gmx.com>
2018-04-17 15:47:56 +0300

    Add salt-call directory parameters

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get -y install git salt-minion
 echo 'master: localhost'|sudo tee /etc/salt/minion
 git clone https://github.com/terokarvinen/sirotin
-sudo salt-call --local --file-root srv/salt/ --pillar-root srv/pillar/
+sudo salt-call --local --file-root sirotin/srv/salt/ --pillar-root sirotin/srv/pillar/
 
 echo "More tips on http://terokarvinen.com/tag/saltstack"
 echo "Done."
